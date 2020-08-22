fdb27f4ba8acfc5e0837fb94859678fb8da6d778
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2020-08-22 16:55:39 +0200

    Add project files to repo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SortingVisualizer} from "./SortingVisualizer/SortingVisualizer";
+
+function App() {
+  return (
+    <SortingVisualizer></SortingVisualizer>
+  );
+}
+
+export default App;
